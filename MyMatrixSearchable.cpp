5c772a0b6771d60371b4cc035b77de071d82fd88
Ariel Yaaran <ariel.yaaran@hotmail.com>
2019-01-16 23:13:39 +0200

    fix another error

--- MyMatrixSearchable.cpp
+++ MyMatrixSearchable.cpp
@@ -9,8 +9,14 @@ vector<State<pair<int, int>>*> MyMatrixSearchable::getPossibleNextStates(State<p
     vector<State<pair<int, int>>*> result;
     int i = current.getState().first;
     int j = current.getState().second;
-    int fatherI = current.getFather()->getState().first;
-    int fatherJ = current.getFather()->getState().second;
+    int fatherI, fatherJ;
+    if (current.getFather() != NULL) {
+        fatherI = current.getFather()->getState().first;
+        fatherJ = current.getFather()->getState().second;
+    }
+    else {
+        fatherI = fatherJ = -1;
+    }
     //if the index exist and the value isn't minus one, which is infinity
     if (i < width-1 && matrixStates[i+1][j] >= 0 && fatherI != i+1) {
         result.push_back(new State<pair<int,int>>(std::make_pair(i + 1, j), current.getCost() + matrixStates[i+1][j], &current, DOWN));
